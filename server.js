006d5affb3cedae3c866f7f8692d693757c4d37b
Colin Moore <cmoore156@gmail.com>
2021-11-13 16:48:02 -0500

    fix

--- server.js
+++ server.js
@@ -53,7 +53,29 @@
   * Event listener for HTTP server "error" event.
   */
  
+ function onError(error) {
+   if (error.syscall !== 'listen') {
+     throw error;
+   }
  
+   var bind = typeof port === 'string'
+     ? 'Pipe ' + port
+     : 'Port ' + port;
+ 
+   // handle specific listen errors with friendly messages
+   switch (error.code) {
+     case 'EACCES':
+       console.error(bind + ' requires elevated privileges');
+       process.exit(1);
+       break;
+     case 'EADDRINUSE':
+       console.error(bind + ' is already in use');
+       process.exit(1);
+       break;
+     default:
+       throw error;
+   }
+ }
  
  /**
   * Event listener for HTTP server "listening" event.
@@ -65,4 +87,3 @@
      : 'port ' + addr.port;
    debug('Listening on ' + bind);
  }
- 
