68d423bca1600515da79b2391c41d648963fe856
Colin Moore <cmoore156@gmail.com>
2021-11-12 21:12:09 -0500

    added home, mysurveys, addSurveys, some routing

---
+++ client/src/app/pages/my-surveys/my-surveys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-surveys',
+  templateUrl: './my-surveys.component.html',
+  styleUrls: ['./my-surveys.component.scss']
+})
+export class MySurveysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
