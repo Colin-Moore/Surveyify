70ca4192c08289752d65d10f17680027cc2d53e6
Melissa Westaway <melissa.f.westaway@gmail.com>
2021-11-13 14:06:58 -0500

    Added survey service file; updated home page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SurveyService } from 'src/app/survey.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private surveyService: SurveyService) { }
 
   ngOnInit(): void {
+    this.surveyService.getSurveys().subscribe((surveys) => {
+      console.log(surveys);
+    })
   }
 
 }
---
+++ client/src/app/survey.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SurveyService {
+
+  uri = 'http://localhost:4000';
+
+  constructor(private http: HttpClient) { }
+
+  getSurveys() {
+    return this.http.get(`${this.uri}/surveys`);
+  }
+
+  getSurveyById(id: any) {
+    return this.http.get(`${this.uri}/surveys/${id}`);
+  }
+
+  addSurvey(surveyName: string, author: string, expirationDate: string) {
+    const survey = {
+      surveyName: surveyName,
+      author: author,
+      expirationDate: expirationDate
+    };
+    return this.http.post(`${this.uri}/surveys/add`, survey);
+  }
+
+  updateSurvey(id: any, surveyName: string, author: string, expirationDate: string) {
+    const survey = {
+      surveyName: surveyName,
+      author: author,
+      expirationDate: expirationDate
+    };
+    return this.http.post(`${this.uri}/surveys/update/${id}`, survey);
+  }
+
+  deleteSurvey(id: any) {
+    return this.http.get(`${this.uri}/surveys/delete/${id}`);
+  }
+}
