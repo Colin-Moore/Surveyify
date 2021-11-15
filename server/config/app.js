9058000326d4a8420d567dd7af2b54e008d1db06
Colin Moore <cmoore156@gmail.com>
2021-11-14 21:13:50 -0500

    put cors fix in right file...

--- server/config/app.js
+++ server/config/app.js
@@ -33,7 +33,12 @@ app.use(express.static(path.join(__dirname, '../..public')));
 app.use(express.static(path.join(__dirname, '../..Assets/images/')));
 app.use(express.static(path.join(__dirname, '../..node_modules')));
 
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+ 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
