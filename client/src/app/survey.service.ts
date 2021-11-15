05e028e759e76b495532931dae530e5a4f338e70
Colin Moore <cmoore156@gmail.com>
2021-11-14 21:12:54 -0500

    cors fix

--- client/src/app/survey.service.ts
+++ client/src/app/survey.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SurveyService {
 
-  uri = 'http://localhost:4000';
+  uri = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 
--- server.js
+++ server.js
@@ -87,3 +87,9 @@
      : 'port ' + addr.port;
    debug('Listening on ' + bind);
  }
+
+ app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
