63f60072795211fd5db908cbe9876142b0aa7654
Colin Moore <66180368+Colin-Moore@users.noreply.github.com>
2021-11-16 15:09:42 -0500

    Delete COMP229-Group1-master/COMP229-Group1-master directory

--- COMP229-Group1-master/COMP229-Group1-master/server/config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  //URI: "mongodb://localhost/surveyify", // connection to local surveyify database,
-  URI: "mongodb+srv://admin:Qw123EF2@group1.2viu9.mongodb.net/Surveyify?retryWrites=true&w=majority",
-};
