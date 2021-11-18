df30a8dda65dbc675940ddfd42faea41cccc7bc7
Eduardo <eduardoescobarjr@outlook.com>
2021-11-18 09:55:34 -0500

    Add Login and Register Page

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ let router = express.Router();
 
 let indexController = require("../controllers/index");
 
+
+
 /* GET home page. */
 router.get("/", indexController.displayHomePage);
 router.get("/home", indexController.displayHomePage);
@@ -12,5 +14,11 @@ router.get("/about", indexController.displayAboutPage);
 
 /* GET contact us page. */
 router.get("/contact", indexController.displayContactPage);
+
+/* GET Login us page. */
+router.get("/login", indexController.displayLoginPage);
+
+/* GET register us page. */
+router.get("/register", indexController.displayRegisterPage);
 
 module.exports = router;
